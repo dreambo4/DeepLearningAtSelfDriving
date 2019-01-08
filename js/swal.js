2a0a3981e44577858c00eb4183461e8e38f61ab8
yjwu <s1041535@gm.pu.edu.tw>
2019-01-08 14:36:26 +0800

    init

---
+++ js/swal.js
@@ -0,0 +1,20 @@
+// opencv intro
+function opencv()
+{
+swal("OpenCV", "提供影像處理或電腦視覺的函式庫，我們會用opencv來進行影像處理。像是圖片的分割處理、灰階調節，或是像我們把拍到的影像呈現在畫面上");
+}
+// yolo intro
+function yolo()
+{
+swal("YOLO", "YOLO主要是做來即時物件的偵測，利用了深度學習讓YOLO準確辨識出物件，並且用方型的框將辨識出的物件框起來");
+}
+//ROS intro
+function ros()
+{
+    swal("ROS", "ROS全名為Robot Operating System也就是機器人作業系統，是專為機器人軟體開發所設計出來的一套電腦作業系統架構。能夠作為程式與程式之間溝通的橋樑");
+}
+//Arduino intro
+function arduino()
+{
+    swal("Arduino", "Arduino的微處理暨和控制器能夠讓我們去控制我們所要控制的硬體設備");
+}
